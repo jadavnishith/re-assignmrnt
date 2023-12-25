7e7142902cebed58a7a37ab31a08f6d4ee16f0c1
jadavnishith <mrnick076@gmail.com>
2023-12-25 11:23:47 +0530

    re-assignment

---
+++ index.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=10;
+    
+    a=20;//re-assignment
+    printf("%d\n",a);
+    
+    
+    float f=10.14;
+    
+    f=20.14;//re-assignment
+    printf("%f\n",f);
+    
+    
+    char c='n';
+    
+    c='t';//re-assignment
+    printf("%c\n",c);
+
+    return 0;
+    
+}
